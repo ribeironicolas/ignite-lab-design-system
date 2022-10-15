fe31c3314b309be7c23558bc1b0657347d54c754
Nicolas-R1beiro <ngcribeiro96@gmail.com>
2022-10-15 14:46:33 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+    <h1 className='text-violet-500 text-2xl'>Hellow World</h1>
+    
+    <button className='bg-cyan-500 '>Enviar</button>
+    </>
+  )
+}
+
